d58b0c95433d06c12457a82728622c4362250f69
Chetan <mohangurjar@gmail.com>
2020-12-30 16:38:25 +0530

    static apges add

--- src/containers/HR/HR.js
+++ src/containers/HR/HR.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import { withRouter } from "react-router-dom";
 
-import salesforce from "../../assets/images/salesforce.png";
-import Ruby from "../../assets/images/Ruby.png";
-import ReactJS from "../../assets/images/ReactJS.png";
-import IOS from "../../assets/images/IOS.png";
-import NodeJS from "../../assets/images/NodeJS.png";
-// import logo from "../../assets/images/logo.png";
 
 
 function HR(props) {
