4b578eca62ad3e7c108ef4a6deebb37a19722afa
Chetan <mohangurjar@gmail.com>
2021-01-18 11:55:40 +0530

    env url

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,2 +1,2 @@
-// export const REACT_API_URL = 'https://oaktreecrew-api.herokuapp.com'
-export const REACT_API_URL = 'http://2ca665b6fcea.ngrok.io'
+export const REACT_API_URL = 'https://oaktreecrew-api.herokuapp.com'
+// export const REACT_API_URL = 'http://00cd3b725014.ngrok.io'
